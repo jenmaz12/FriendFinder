e0e7f909d2b9730b62bb081f93e7cb7adf36537e
jenmaz12 <jenmaz12@gmail.com>
2019-02-07 13:12:34 -0500

    no errors no modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,44 @@
 var friendData = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+function apiRoutes(app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendData);
     });
 
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
+        let newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: []
+        };
+
+        for (let j = 0; j < req.body.scores.length; j++) {
+            newFriend.scores.push(parseInt(req.body.scores[j]))
+        }
+
+        let totalDiffs = [];
+
+        for (j = 0; j < friendData.length; j++) {
+            let diffSum = 0;
+            for (let i = 0; i < newFriend.scores.length; i++) {
+                diffSum += Math.abs(friendData[j].scores[i] - newFriend.scores[i]);
+            };
+            totalDiffs.push(diffSum);
+        }
+
+        let friendMatchIndex = 0;
+        for (let i = 1; i < totalDiffs.length; i++) {
+            if (totalDiffs[i] <= totalDiffs[friendMatchIndex]) {
+                friendMatchIndex = i;
+            }
+        }
+
+        let friendMatch = friendData[friendMatchIndex];
+        res.json(friendMatch);
+
         friendData.push(req.body);
-        res.json(req.body);
+
     });
 };
+
+module.exports = apiRoutes;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,13 @@
 var path = require("path");
 
-module.exports = function(app) {
+function htmlRoutes(app) {
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
-    app.get("*", function(req, res) {
+    app.use(function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
+
+module.exports = htmlRoutes;
