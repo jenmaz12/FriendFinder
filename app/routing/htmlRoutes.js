17ce7eb8298d0e5604824b72b1ed289ed0b20d6c
jenmaz12 <jenmaz12@gmail.com>
2019-02-07 17:53:41 -0500

    fixed route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,7 @@ function htmlRoutes(app) {
     });
 
     app.get("/", function(req, res) {
-        res.json(path.join(__dirname, "public/home.html"));
+        res.json(path.join(__dirname, "../public/home.html"));
       });      
 
     app.use(function(req, res) {
