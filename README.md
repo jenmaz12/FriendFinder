afe22ab16fafb5aae523effc59ff9b27d62a197a
jenmaz12 <jenmaz12@gmail.com>
2019-02-08 07:02:59 -0500

    more friend data and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # FriendFinder
+FriendFinder is an application which, using Node.js and Express, returns a friend match based on the user's answers to a survey.
+
+Visit the deployed site on Heroku [here](https://sleepy-springs-62403.herokuapp.com/).
+
+This application uses an API route to POST the user's survey results to a data JavaScript file and return a friend from that data set with the least amount of total difference between their scores on the questions.
+
+The application also allows the user to view all of friends in the data set by using a GET API route.
+
+The HTML files are displayed using GET routes to the HTML file. 
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,34 @@
-var friendArray = [];
+var friendArray = [
+    {
+        name: "Hermione Granger",
+        photo: "https://vignette.wikia.nocookie.net/harrypotter/images/4/41/Hermionedhface.jpg/revision/latest?cb=20161221044857",
+        scores: [3,5,4,3,1,2,3,3,2,5]
+    },
+    {
+        name: "Kermit the Frog",
+        photo: "http://media.nola.com/entertainment_impact_tvfilm/photo/10267535-large.jpg",
+        scores: [2,4,3,1,1,2,5,1,2,3]
+    },
+    {
+        name: "Steve Rogers",
+        photo: "https://media.comicbook.com/2017/11/steve-rogers-1060339-1280x0.jpg",
+        scores: [1,5,3,1,5,2,2,1,4,4]
+    },
+    {
+        name: "Jack Sparrow",
+        photo: "https://i.pinimg.com/originals/7e/82/01/7e8201cb1c5cca7c0d981b7d8ce29f89.jpg",
+        scores: [2,2,4,3,1,5,5,2,3,4]
+    },
+    {
+        name: "Bugs Bunny",
+        photo: "https://vignette.wikia.nocookie.net/scrooge-mcduck/images/8/81/BugsBunny.png/revision/latest?cb=20180502122014",
+        scores: [3,1,4,2,1,1,5,4,4,2]
+    },
+    {
+        name: "Jon Snow",
+        photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/08/20/16/jon-snow.jpg?w968h681",
+        scores: [2,3,2,4,4,5,1,1,3,4]
+    }
+];
 
 module.exports = friendArray;
